68f616ea07e84dda25aa4964782330dde7e7c7a1
Kenner Gutierrez Vasquez <kgutierrez@atesa.local>
2026-03-27 21:14:53 -0600

    Fix: add site and base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://KennerGV.github.io',   
+  base: '/software_engineer_kengv',
+});
